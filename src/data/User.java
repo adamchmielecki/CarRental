bcbdba16aca0d59a7751108f6c61879ecd401841
adamchmielecki <chmielu.adam@gmail.com>
2020-11-20 01:26:40 +0100

    Initial commit

---
+++ src/data/User.java
@@ -0,0 +1,95 @@
+package data;
+
+public class User {
+    int ID;
+    int PESEL;
+    String firstName;
+    String lastName;
+    int phoneNumber;
+    int postCode;
+    String city;
+    String address;
+    int idCardNumber;
+    int drivingLicenseNumber;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public int getPESEL() {
+        return PESEL;
+    }
+
+    public void setPESEL(int PESEL) {
+        this.PESEL = PESEL;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getPostCode() {
+        return postCode;
+    }
+
+    public void setPostCode(int postCode) {
+        this.postCode = postCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getIdCardNumber() {
+        return idCardNumber;
+    }
+
+    public void setIdCardNumber(int idCardNumber) {
+        this.idCardNumber = idCardNumber;
+    }
+
+    public int getDrivingLicenseNumber() {
+        return drivingLicenseNumber;
+    }
+
+    public void setDrivingLicenseNumber(int drivingLicenseNumber) {
+        this.drivingLicenseNumber = drivingLicenseNumber;
+    }
+
+}
